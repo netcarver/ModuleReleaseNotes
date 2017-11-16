552c3ba362c5608c8f1a39fd8620acfdec7c1736
Netcarver <id.github.com@gzap.org>
2017-11-16 03:10:09 +0000

    Improve changelog division & add height control to file display

--- ModuleReleaseNotes.js
+++ ModuleReleaseNotes.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("a.grow-height").click(function() {
     $(this).closest('.expansion-wrapper').children('.expand').animate({'height': '+=400px'}, 500);
   });
+
+
+  $("a.full-height").click(function() {
+    $(this).closest('.expansion-wrapper').children('.expand').animate({'height': '100%'}, 500);
+    $(this).hide();
+  });
 });
