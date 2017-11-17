6b4bc2dfc471e7ea617ce9c239bd8a9693338c52
Netcarver <id.github.com@gzap.org>
2017-11-17 16:17:50 +0000

    Auto trim div.long blocks with short content.

--- ModuleReleaseNotes.js
+++ ModuleReleaseNotes.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+
+  /**
+   * If the content of the expand box is likely less than the initially assigned height, set the height to auto and
+   * remove the show-all link.
+   */
+  $('div.long').each(function() {
+    $this = $(this);
+    var content = $this.html();
+
+    max_len = 1024;
+    if (content.length <= max_len) {
+      $this.height('100%');
+      $this.next().hide();
+    }
+  });
+
+
   $("a.grow-height").click(function() {
     $(this).closest('.expansion-wrapper').children('.expand').animate({'height': '+=400px'}, 500);
   });
@@ -8,4 +25,6 @@ $(document).ready(function() {
     $(this).closest('.expansion-wrapper').children('.expand').animate({'height': '100%'}, 500);
     $(this).hide();
   });
+
+
 });
