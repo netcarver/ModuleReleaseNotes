e65e1b2257332e4b8dbc3a729df770a8fcf812d4
Netcarver <id.github.com@gzap.org>
2024-05-21 21:58:55 +0100

    fix: use .on('click' fn() {})

--- ModuleReleaseNotes.js
+++ ModuleReleaseNotes.js
@@ -1,30 +1,31 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   /**
    * If the content of the expand box is likely less than the initially assigned height, set the height to auto and
    * remove the show-all link.
    */
-  $('div.long').each(function() {
+  $("div.long").each(function () {
     $this = $(this);
     var content = $this.html();
 
     max_len = 1024;
     if (content.length <= max_len) {
-      $this.height('100%');
+      $this.height("100%");
       $this.next().hide();
     }
   });
 
-
-  $("a.grow-height").click(function() {
-    $(this).closest('.expansion-wrapper').children('.expand').animate({'height': '+=400px'}, 500);
+  $("a.grow-height").on("click", function () {
+    $(this)
+      .closest(".expansion-wrapper")
+      .children(".expand")
+      .animate({ height: "+=400px" }, 500);
   });
 
-
-  $("a.full-height").click(function() {
-    $(this).closest('.expansion-wrapper').children('.expand').animate({'height': '100%'}, 500);
+  $("a.full-height").on("click", function () {
+    $(this)
+      .closest(".expansion-wrapper")
+      .children(".expand")
+      .animate({ height: "100%" }, 500);
     $(this).hide();
   });
-
-
 });
